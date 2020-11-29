1c16c8c2664cfe3310c86c92b36428767a9ea838
kacperkapusciak <kapusciak.k@gmail.com>
2020-11-29 16:15:42 +0100

    fix: set port dynamically

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use(cors());
 
 require('./routes')(app);
 
-const port = 7000;
+const port = process.env.PORT || 7000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = server;
